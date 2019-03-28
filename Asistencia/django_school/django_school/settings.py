e56dfcc0b71b8a37b3bd736b26fc26296be23102
adityacheruvu <adityacheruvucs@gmail.com>
2019-03-29 00:14:08 +0530

    Changed DB password

--- Asistencia/django_school/django_school/settings.py
+++ Asistencia/django_school/django_school/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'asistencia',
         'USER': 'root',
-        'PASSWORD': 'nani@712',
+        'PASSWORD': 'toor',
         'HOST': '127.0.0.1',
         'PORT': '3306'
         }
